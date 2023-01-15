35650ce74b5080ba29d1920af63390b9deb7b75a
Jerrorld <jerrorldk@gmail.com>
2023-01-14 17:23:57 -0800

    added static files and styling

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -5,12 +5,14 @@ def create_app():
     app = Flask(__name__)
 
     @app.route('/')
-    def hello(): 
+    def index(): 
         return 'Hello, PetFax!'
 
     from . import pet
     app.register_blueprint(pet.bp)
 
+    from . import fact
+    app.register_blueprint(fact.bp)
+
     #return the app
     return app
-
